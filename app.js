536e124073a5f479a363be3167ebf0d6e825c180
Alif Corrêa Flores <alif.flores@credpago.com>
2021-10-11 14:31:32 -0300

    separando routes de views

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin', adminRouter);
+app.use(shopRouter);
+
+app.use((req, res)=>{
+    res.status(404).send('<h1> Page Not Found</h1>');
+});
+
+app.listen(3000);
